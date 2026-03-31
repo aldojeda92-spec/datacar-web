de1021bb66d65fc082440d4e9263aeb7f0da2116
aldojeda92-spec <aldojeda92@gmail.com>
2026-03-31 15:16:09 +0000

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     try {
         const { message } = req.body;
         // 1. TU LLAVE ACÁ
-        const apiKey = "AIzaSyBnp938UgpTbF6uA-zu2zbGw4m7RN9Ubuo".trim();
+        const apiKey = "AIzaSyDDq2-433ObsJ9rL9BrLi5Pb10n8tA3Ckk".trim();
         const genAI = new GoogleGenerativeAI(apiKey);
         const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
