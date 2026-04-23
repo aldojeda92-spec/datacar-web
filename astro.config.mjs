bcfc64d1ab4cea28a749adf5fd49238487266be9
aldojeda92-spec <aldojeda92@gmail.com>
2026-04-23 20:36:11 -0300

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  // 'server' permite que la web sea dinámica (necesario para la base de datos)
+  output: 'server', 
+  // Adaptador oficial para que Vercel entienda el proyecto
+  adapter: vercel(),
+  // Inyectamos Tailwind para el diseño
   integrations: [tailwind()],
 });
