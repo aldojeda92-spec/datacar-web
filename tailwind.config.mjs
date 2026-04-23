af9406273e59b8fd73d76a240fe1bbb41d38ce7f
aldojeda92-spec <aldojeda92@gmail.com>
2026-04-23 20:37:02 -0300

    ttailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors: {
-				// Colores Oficiales Manual DATACAR
-				primary: '#0A1F33',   // Authority Blue (Fondo, Seriedad)
-				secondary: '#3A3A3C', // Data Charcoal (Textos)
-				accent: '#00BFFF',    // Digital Cyan (Botones/Acción)
-			},
 			fontFamily: {
-				// Tipografías Oficiales
-				sans: ['Inter', 'sans-serif'],    // Lectura
-				heading: ['Archivo', 'sans-serif'], // Impacto
+				montserrat: ['Montserrat', 'sans-serif'],
+				inter: ['Inter', 'sans-serif'],
 			},
 		},
 	},
